b53a42edc58cc4955e8c18f4f52620ef13b78a65
cg <cg@cryptng.com>
2024-01-15 11:14:30 +0100

    Feat: rename

--- ai-term.js
+++ ai-term.js
@@ -14,14 +14,54 @@ tpl1.innerHTML=`<div style="display: inline-flex;">
 const tpl2 = document.createElement('template');
 tpl2.id='ai-term-chat';
 tpl2.innerHTML=`<div id="ai-term-chat-window">
-<div class="title">AI Wiki Chat</div>
+<div class="title"><svg
+width="120"
+viewBox="0 0 76.231392 11.96944"
+version="1.1"
+id="svg5"
+xmlns="http://www.w3.org/2000/svg"
+xmlns:svg="http://www.w3.org/2000/svg">
+
+<g
+  id="layer1"
+  transform="translate(-33.29779,-110.10422)">
+ <g
+    id="g1245">
+   <text
+      xml:space="preserve"
+      style="font-weight:300;font-size:16.9333px;font-family:Quicksand;fill:#1984ef;fill-opacity:1;stroke-width:0.442;stroke-linecap:round"
+      x="42.026588"
+      y="121.90433"
+      id="text236"><tspan
+        id="tspan234"
+        style="fill:#1984ef;fill-opacity:1;stroke-width:0.442"
+        x="42.026588"
+        y="121.90433"><tspan
+          style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:16.9333px;font-family:Quicksand;font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;font-variation-settings:'wght' 500;fill:#1984ef;fill-opacity:1"
+          id="tspan340">ai-term</tspan></tspan></text>
+   <path
+      style="font-variation-settings:'wght' 500;fill:none;stroke-width:1;stroke-linecap:round;stroke-dasharray:none"
+      d="m 39.938875,110.10422 -6.33762,3.65902 6.363017,3.67369"
+      id="path469" />
+   <path
+      style="font-variation-settings:'wght' 500;fill:none;stroke:#ffffff;stroke-width:1.3;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1"
+      d="m 40.392726,113.73502 -6.445001,3.72102 6.44548,3.72131"
+      id="path1133" />
+   <path
+      style="font-variation-settings:'wght' 500;fill:none;stroke:#ffffff;stroke-width:1.3;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1"
+      d="m 102.43425,113.82621 6.445,3.72102 -6.44548,3.72131"
+      id="path1133-3" />
+ </g>
+</g>
+</svg>
+</div>
 <form>
 <label for="ai-term-prompt">Ask Prompt</label>
 <div class="prompt-ask">
   <input id="ai-term-prompt"/>
   <button id="ai-term-ask">Ask AI</button>
 </div>
-<label for="ai-term-bot">Wiki AI Answer</label>
+<label for="ai-term-bot">Ai Term Answer</label>
 <div class="textarea" id="ai-term-bot"></div>
 </form>
 </div>`;
@@ -99,7 +139,7 @@ class AiTermChat extends HTMLElement {
           width: 400px;
           background: white;
           position: absolute;
-          bottom: -410px;
+          bottom: 0px;
           right: 0vw;
           display:flex;
           flex-direction: column;
@@ -118,8 +158,8 @@ class AiTermChat extends HTMLElement {
           width: 100%;
           height: 2rem;
           border-radius: 20px 20px 0 0;
-          background-color: burlywood;
-          padding: 10px 0 10px 30px;
+          background-color: black;
+          padding: 14px 0 14px 30px;
           color: black;
           font-size: large;
           box-shadow: 5px 5px 10px #0000005c;
