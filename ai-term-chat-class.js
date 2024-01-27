5196d02bc84f44e389651c92680590be6e1989de
cg <cg@cryptng.com>
2024-01-27 21:29:24 +0100

    fix: use correct file versions

--- ai-term-chat-class.js
+++ ai-term-chat-class.js
@@ -1,28 +1,30 @@
 
+async function Sleep(milliseconds) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
 export default class AiTermChat extends HTMLElement {
     minify(event){
+      event.preventDefault();
       event.target.closest('#ai-term-chat-window').classList.add('min-size');
       event.target.closest('#ai-term-chat-window').classList.remove('max-size');
       event.target.closest('#ai-term-chat-window').classList.remove('mid-size');
     }
     midify(event){
+      event.preventDefault();
       event.target.closest('#ai-term-chat-window').classList.add('mid-size');
       event.target.closest('#ai-term-chat-window').classList.remove('max-size');
       event.target.closest('#ai-term-chat-window').classList.remove('min-size');
     }
 
     maxify(event){
+      event.preventDefault();
       event.target.closest('#ai-term-chat-window').classList.add('max-size');
       event.target.closest('#ai-term-chat-window').classList.remove('mid-size');
       event.target.closest('#ai-term-chat-window').classList.remove('min-size');
     }
 
-    close(event){
-      event.target.closest('#ai-term-chat-window').classList.remove('active');
-      event.target.closest('#ai-term-chat-window').classList.remove('max-size');
-      event.target.closest('#ai-term-chat-window').classList.remove('min-size');
-      event.target.closest('#ai-term-chat-window').classList.add('mid-size');
-    }
+    
     constructor() {
         super();
         this.apiKey='';
@@ -43,25 +45,101 @@ export default class AiTermChat extends HTMLElement {
 
         const style = document.createElement('style');
         style.textContent = `
+        @media (max-width: 800px)
+        {
+          .backdrop{
+            width:100dvw;
+            height:100dvh;
+            position:fixed;
+            top: 100dvh;
+            visibility: hidden;
+            background-color: #aeaeae6b;
+            backdrop-filter: blur(3px);
+          }
+
+          .backdrop.active{
+            top:0;
+            visibility:visible;
+          }
+        }
+
+        @media (min-width: 800px)
+        {
+          .backdrop{
+            display:none;
+          }
+        }
+        
+
+        
+        
+
+        @media (max-width: 800px)
+        {
+
+          #ai-term-chat-window.active {
+            display:flex;
+          }
+
+          
+          #ai-term-chat-window.active.visible {
+            opacity:1;
+          }
+
+          #ai-term-chat-window {
+            top:calc(50dvh - 200px);
+            left:calc(50dvw - 200px);
+            border-radius: 20px;
+            display:none;
+            opacity:0;
+          }
+
+          #ai-term-chat-window.max-size{
+            top:45px;
+          }
+        }
+
+        @media (min-width: 800px)
+        {
+          #ai-term-chat-window.active {
+            display:flex;
+          }
+
+          
+          #ai-term-chat-window.active.visible {
+            bottom:0px;
+          }
+
+          #ai-term-chat-window {
+            bottom:-440px;
+            right:0;
+            border-radius: 20px 20px 0 0;
+            display:flex;
+          }
+
+          
+
+        }
         #ai-term-chat-window {
-          z-index: 100;
+          z-index: 9999999999;
+          
           height: 400px;
           width: 400px;
           background: white;
           position: fixed;
-          bottom: -410px;
-          right: 0vw;
           display:flex;
           flex-direction: column;
           flex-wrap: nowrap;
           align-items: center;
           justify-content: flex-start;
-          border-radius: 20px 20px 0 0;
+    
           padding: 0px;
           box-shadow: -5px 5px 10px #00000085;
           transition:all 0.4s ease-in-out;
           overflow: hidden;
           color:black;
+          
+          transition: all 0.5s ease-in-out;
         }
 
         #ai-term-chat-window.mid-size{
@@ -70,33 +148,40 @@ export default class AiTermChat extends HTMLElement {
 
         #ai-term-chat-window.min-size{
           height: 50px;
+          width:50px;
+          right:0;
         }
 
         #ai-term-chat-window.max-size{
           height: calc(100dvh - 95px);
+        
         }
 
         #ai-term-chat-window .title {
-          width: calc(100% - 50px);
+          width: calc(100% - 35px);
           border-radius: 20px 20px 0 0;
-          background-color: black;
-          padding: 14px 20px 14px 30px;
+          padding: 2px 5px 4px 30px;
           color: black;
           font-size: large;
-          box-shadow: 5px 5px 10px #0000005c;
+          font-weight: 600;
           display: flex;
           flex-direction: row;
           flex-wrap: nowrap;
           justify-content: space-between;
           align-items: center;
-          color: white;
+          color: black;
+        }
+
+        #ai-term-chat-window .title .window-buttons{
+          position:relative;
+          right:-5px;
         }
 
         #ai-term-chat-window .title .window-buttons .span{
           cursor:pointer;
         }
 
-        #ai-term-chat-window.active {
+        .backdrop.active #ai-term-chat-window {
           bottom: 0vh;
         }
 
@@ -120,55 +205,65 @@ export default class AiTermChat extends HTMLElement {
           display: flex;
           flex-direction: row;
           flex-wrap: nowrap;
-          margin-bottom:20px;
-          position: relative;
+          position: absolute;
+          bottom: 10px;
+          width: 350px;
         }
 
         #ai-term-chat-window form input {
           width: 100%;
           line-height: 1.3rem;
-          border-radius: 10px;
-          border: 1px solid cornflowerblue;
+          border: 0;
+          border-bottom: 1px solid cornflowerblue;
           height: 2.5rem;
           padding-left: 10px;
           padding-right: 50px;
+          outline: none;
         }
 
         #ai-term-chat-window form input:focus {
-          border: 2px solid cornflowerblue;
+          outline: none;
+          border-bottom: 2px solid cornflowerblue;
+        }
+
+        input#ai-term-prompt:focus-visible {
+          outline: none;
+        }
+        input#ai-term-prompt:-internal-autofill-selected {
+          background-color:transparent;
         }
 
         #ai-term-chat-window form button {
           line-height:2em;
-          border-radius: 10px;
-          background-color: #c0d7ff;
-          border: 2px solid cornflowerblue;
-          box-shadow: 5px 5px 8px #36568f59;
+          background-color: transparent;
+          border:0;
           cursor: pointer;
           position: absolute;
-          right: 6px;
-          top: 5px;
+          right: -6px;
+          top: 12px;
         }
 
-        #ai-term-chat-window form button:hover {
+        #ai-term-chat-window form button:hover svg path{
       
-          background-color: #c0d7ffa0;
-          box-shadow: 5px 5px 8px #36568f69;
+          stroke:#4675c9 !important;
    
         }
 
         #ai-term-chat-window form .textarea {
           height: 240px;
           line-height:1.5em;
-          margin-bottom:20px;
+          margin-bottom:9px;
           border-radius:10px;
           border: 0px solid cornflowerblue;
           color: black;
           padding: 5px;
           overflow-x: hidden;
-          overflow-y: scroll;
+          overflow-y: auto;
         }
 
+        #ai-term-chat-window form .textarea b{
+          color:#4675c9;
+        }
         #ai-term-chat-window.max-size form .textarea{
           height:calc(80dvh - 2.5rem - 5dvh);
         }
@@ -191,20 +286,48 @@ export default class AiTermChat extends HTMLElement {
         const input = shadowRoot.querySelector('input');
         const textarea = shadowRoot.querySelector('.textarea');
 
-        shadowRoot.querySelector('.window-buttons .minify').addEventListener('click', this.minify);
-        shadowRoot.querySelector('.window-buttons .midify').addEventListener('click', this.midify);
-        shadowRoot.querySelector('.window-buttons .maxify').addEventListener('click', this.maxify);
-        shadowRoot.querySelector('.window-buttons .close').addEventListener('click', this.close);
+        
+        
 
         document.querySelector('body').aiTermInputRef = shadowRoot.querySelector('input');
+        const chatW = shadowRoot.querySelector('#ai-term-chat-window');
         document.querySelector('body').aiTermChatWindowRef = shadowRoot.querySelector('#ai-term-chat-window');
+        chatW.show = async ()=>{
+          chatW.classList.add('active');
+          shadowRoot.querySelector('.backdrop').classList.add('active');
+          await Sleep(50);
+          chatW.classList.add('visible');
+        };
+
+        chatW.close = async ()=>{
+    
+            chatW.classList.remove('visible');
+            await Sleep(500);
+            shadowRoot.querySelector('.backdrop').classList.remove('active');
+            chatW.classList.remove('active');
+            chatW.classList.remove('max-size');
+            chatW.classList.remove('min-size');
+            chatW.classList.add('mid-size');
+         
+        };
+
+        //shadowRoot.querySelector('.window-buttons .minify').addEventListener('click', this.minify);
+        //shadowRoot.querySelector('.window-buttons .midify').addEventListener('click', this.midify);
+        //shadowRoot.querySelector('.window-buttons .maxify').addEventListener('click', this.maxify);
+        shadowRoot.querySelector('.window-buttons .close').addEventListener('click', async (event)=>{
+          chatW.close();
+        });
+
+        shadowRoot.querySelector('.backdrop').onclick = async (event)=>{
+          chatW.close();
+        };
         document.querySelector('body').aiTermButtonRef = shadowRoot.querySelector('button');
 
         var form = shadowRoot.querySelector("form");
         function handleForm(event) { event.preventDefault(); } 
-        form.addEventListener('submit', async (event)=>{
-          event.preventDefault();
-
+        document.querySelector('body').aiTermButtonRef.onclick = async (event)=>{
+         // event.stopPropagation();
+         event.preventDefault();
           let _apiKey=this.apiKey;
           if(_apiKey===''){
             const meta = document.querySelector('head meta[name="ai-term_apiKey"]');
@@ -239,7 +362,7 @@ export default class AiTermChat extends HTMLElement {
 
           let div = document.createElement('div');
 
-          div.innerHTML=`<b>Me</b>: ${input.value}`;
+          div.innerHTML=`<b>${input.value}</b>`;
           
 
           textarea.appendChild(div);
@@ -269,14 +392,15 @@ export default class AiTermChat extends HTMLElement {
               }) 
             });
             const resp = (await response.json()).response;
-            div.innerHTML=`<b>AI</b>: ${resp}`;
+            div.innerHTML=`<em>${resp}</em>`;
             
           }catch(e){
             div.textContent='AI is overworked :-(';
           }
           input.value='';
           textarea.scrollTop = textarea.scrollHeight;
-        });
+        };
+        
     }
 
     static get observedAttributes() {
@@ -289,4 +413,3 @@ export default class AiTermChat extends HTMLElement {
 
 
 }
-  
--- ai-term-class.js
+++ ai-term-class.js
@@ -2,45 +2,45 @@
 
 export default class AiTerm extends HTMLElement {
           
-    constructor() {
-        super();
-        this.prompt = '';
-        const template = document.getElementById('ai-term');
-        const templateContent = template.content;
-        //const span = document.createElement('span');
-        //const slot = document.createElement('slot');
-        //span.classList="ai-term";
-        //span.appendChild(slot);
-        //templateContent.appendChild(span)
+  constructor() {
+      super();
+      this.prompt = '';
+      const template = document.getElementById('ai-term');
+      const templateContent = template.content;
+      //const span = document.createElement('span');
+      //const slot = document.createElement('slot');
+      //span.classList="ai-term";
+      //span.appendChild(slot);
+      //templateContent.appendChild(span)
 
-        const shadowRoot = this.attachShadow({mode: 'open'});
+      const shadowRoot = this.attachShadow({mode: 'open'});
 
-        const style = document.createElement('style');
-        style.textContent = `
-            span.ai-term { padding: 0px;border-bottom: 0.1em dashed color(display-p3 1.000000 0.270588 0.000000 / 0.701961);cursor: pointer;}
-        `;
+      const style = document.createElement('style');
+      style.textContent = `
+          span.ai-term { padding: 0px;border-bottom: 0.1em dashed color(display-p3 1.000000 0.270588 0.000000 / 0.701961);cursor: pointer;}
+      `;
 
-        
-        shadowRoot.appendChild(style);
-        shadowRoot.appendChild(templateContent.cloneNode(true));
-        
+      
+      shadowRoot.appendChild(style);
+      shadowRoot.appendChild(templateContent.cloneNode(true));
+      
 
-        shadowRoot.querySelector('.ai-term').addEventListener('click', (event) =>{
-          event.stopPropagation();
-          document.querySelector('body').aiTermInputRef.value=(this.prompt||null)===null?this.textContent:this.prompt;
-          document.querySelector('body').aiTermChatWindowRef.show();
-          document.querySelector('body').aiTermButtonRef.click();
+      shadowRoot.querySelector('.ai-term').addEventListener('click', (event) =>{
+        event.stopPropagation();
+        document.querySelector('body').aiTermInputRef.value=(this.prompt||null)===null?this.textContent:this.prompt;
+        document.querySelector('body').aiTermChatWindowRef.show();
+        document.querySelector('body').aiTermButtonRef.click();
 
-        });
-    }
+      });
+  }
 
-    static get observedAttributes() {
-      return ["prompt"];
-    }
-    attributeChangedCallback(name, oldValue, newValue) {
-      if(name==='prompt') this.prompt = newValue;
-    }
+  static get observedAttributes() {
+    return ["prompt"];
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if(name==='prompt') this.prompt = newValue;
+  }
 
 
 }
-    
+  
